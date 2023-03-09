f82eabe251d068de55e57fed95cae2ef6d01c2a0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-09 01:21:12 +0100

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
-function App() {
+import { createSignal } from "solid-js";
+
+function TodoApp() {
+  const [todos, setTodos] = createSignal([]);
+  const [newTodo, setNewTodo] = createSignal("");
+
+  function addTodo() {
+    if (newTodo()) {
+      setTodos([...todos(), { text: newTodo(), completed: false }]);
+      setNewTodo("");
+    }
+  }
+
+  function deleteTodo(index) {
+    const newTodos = [...todos()];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  function toggleCompleted(index) {
+    const newTodos = [...todos()];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
   return (
     <div>
-      <header>
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodo()}
+        onInput={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos().map((todo, index) => (
+          <li>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleCompleted(index)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoApp;
