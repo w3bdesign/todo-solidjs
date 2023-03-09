c642c82b5526495a224ed286d0a71e02fbe851de
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-09 01:19:59 +0100

    Start of project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
